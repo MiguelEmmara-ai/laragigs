cec730558968bbfd32b6e9343b87b5eb608673be
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2022-12-08 14:02:27 +1300

    Adding the Theme HTML, and partials

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingsController;
+use App\Models\Listings;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.listings', [
+        'heading' => 'Latest Listings',
+        'listings' => Listings::all()
+    ]);
+})->name('home');
 
 Route::resource('listings', ListingsController::class);
